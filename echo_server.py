6bb7960f839e67d4f0b5a0d9a2410d8b98f4b779
ybekele <yonael@ualberta.ca>
2019-01-20 16:31:20 -0700

    Update echo_server.py

--- echo_server.py
+++ echo_server.py
@@ -1,42 +1,36 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
-import socket 
-import time 
+import socket
+import time
 
-
-HOST = "" 
+HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
-
 
 def main():
     #create socket 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
+        #allows to reuse same bind port
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)      
 
-	# allows to reuse same bind port 
-        s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
-        s.listen(1) #make socket listen 
-        #conn, addr = s.accept() #accept incoming connections
-        #print(addr) 
-        #full_data = b""
-
-        # makes it listen to forever 
+        s.listen(1) #make socket listen
+       
+        #listen forever for connections
         while True:
-        conn, addr = s.accept() #accept incoming connections
-        print(addr)
-        full_data = b""
+            conn, addr = s.accept() #accept incoming connections
+            print(addr)
+            full_data = b""
             while True:
-                data = conn.recv(BUFFER_SIZE) 
+                data = conn.recv(BUFFER_SIZE)
                 if not data:
                     break
-                full_data += data 
-        #print(full_data)
-        #send data back as response
-                time.sleep(0.5) 
-                conn.sendall(full_data)
-
+                full_data += data
+            #print(full_data)
+            #send data back as response
+            time.sleep(0.5)
+            conn.sendall(full_data)
+ 
 
 if __name__ == "__main__":
     main()
